510efba856af09ad2286fb714be1ae3bc0116951
Sivasankaran <sivasankaranit44@gmail.com>
2026-09-03 10:57:11 +0530

    simplified server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-const dns = require("dns");
-
-dns.setServers(["1.1.1.1", "1.0.0.1"])
-
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
-const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 const authRoutes = require("./routes/authRoutes");
-const clientRoutes = require("./routes/clientRoutes");
-const invoiceRoutes = require("./routes/invoiceRoutes");
 
 const app = express();
 
@@ -24,11 +17,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/clients", clientRoutes);
-app.use("/api/invoices", invoiceRoutes);
-
-app.use(notFound);
-app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
